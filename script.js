0f4b3da18a168845379b505295fba40d2b95236b
Andrew Kolb <akolb87@gmail.com>
2018-10-30 20:37:10 -0500

    display functionality

--- script.js
+++ script.js
@@ -5,11 +5,35 @@
 function submitForm() {
     $('form').submit(event => {
         event.preventDefault();
-        let searchTerm = $('#js-search-term').val();
-        console.log(`current search term is ${searchTerm}`);
-        let endpoint = "https://api.github.com/users/" + searchTerm + "/repos";
-        console.log(`current endpoint is ${endpoint}`);
-    });
+        const searchTerm = $('#js-search-term').val();
+        const endpoint = "https://api.github.com/users/" + searchTerm + "/repos";
+
+        fetch(endpoint)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.message);
+            })
+            .then(responseJson => displayResults(responseJson))
+            .catch(err => {
+                console.log(`${err.message}`);
+                $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            });
+    })
 }
 
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.length; i++){
+    $('#results-list').append(
+      `<li><h3><a href="${responseJson[i].url}">${responseJson[i].name}</a></h3>
+      </li>`
+    )};
+  $('#results').removeClass('hidden');
+};
+
 $(submitForm);
+
+
